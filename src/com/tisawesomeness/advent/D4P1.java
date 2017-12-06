2495d524ad75c004a34c1016e87a0c2364a26eeb
Tisawesomeness <tis@creeperscreep.com>
2017-12-06 13:40:48 -0500

    Day 4

---
+++ src/com/tisawesomeness/advent/D4P1.java
@@ -0,0 +1,37 @@
+package com.tisawesomeness.advent;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class D4P1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String input = "";
+		while (scan.hasNextLine()) {
+		    String line = scan.nextLine();
+		    if (line.isEmpty()) break;
+		    input += line + "\n";
+		}
+		scan.close();
+		
+		int count = 0;
+		String[] lines = input.split("\\n");
+		for (String s : lines) {
+			if (allUnique(s.split(" "))) count++; //If password is valid
+		}
+		System.out.println(count);
+	}
+	
+	//Tests if every word is unique, by using properties of the Set class
+	public static boolean allUnique(String[] arr) {
+		Set<String> lump = new HashSet<String>();
+	  	for (String s : arr) {
+	  		if (lump.contains(s)) return false;
+		  	lump.add(s);
+	  	}
+	  	return true;
+	}
+
+}
---
+++ src/com/tisawesomeness/advent/D4P2.java
@@ -0,0 +1,42 @@
+package com.tisawesomeness.advent;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class D4P2 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String input = "";
+		while (scan.hasNextLine()) {
+		    String line = scan.nextLine();
+		    if (line.isEmpty()) break;
+		    input += line + "\n";
+		}
+		scan.close();
+		
+		int count = 0;
+		String[] lines = input.split("\\n");
+		for (String s : lines) {
+			if (allUnique(s.split(" "))) count++;
+		}
+		System.out.println(count);
+	}
+	
+	//Tests if every alphabetized word is unique
+	public static boolean allUnique(String[] arr) {
+		Set<String> lump = new HashSet<String>();
+	  	for (String s : arr) {
+	  		char[] chars = s.toCharArray();
+	  		Arrays.sort(chars);
+	  		s = new String(chars);
+	        System.out.println(s);
+	  		if (lump.contains(s)) return false;
+		  	lump.add(s);
+	  	}
+	  	return true;
+	}
+
+}
