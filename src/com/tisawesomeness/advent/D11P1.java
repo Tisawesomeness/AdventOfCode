bfeccb852953f708c360b1ed8d95361a4b810fe7
Tisawesomeness <tis@creeperscreep.com>
2017-12-11 22:29:38 -0500

    Day 11

---
+++ src/com/tisawesomeness/advent/D11P1.java
@@ -0,0 +1,55 @@
+package com.tisawesomeness.advent;
+
+import java.util.Scanner;
+
+public class D11P1 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		scan.close();
+		
+		//Follow directions in input
+		int x = 0;
+		int y = 0;
+		for (String s : input.split(",")) {
+			switch (s) {
+				case "nw": x--; if (Math.abs(x) % 2 == 0) y++; break;
+				case "n": y++; break;
+				case "ne": x++; if (Math.abs(x) % 2 == 0) y++; break;
+				case "sw": x--; if (Math.abs(x) % 2 == 1) y--; break;
+				case "s": y--; break;
+				case "se": x++; if (Math.abs(x) % 2 == 1) y--;
+			}
+		}
+		//Go to x = 0
+		int count = 0;
+		while (x != 0) {
+			if (x > 0) {
+				if (y > 0) {
+					x--; if (Math.abs(x) % 2 == 1) y--; //sw
+				} else {
+					x--; if (Math.abs(x) % 2 == 0) y++; //nw
+				}
+			} else {
+				if (y > 0) {
+					x++; if (Math.abs(x) % 2 == 1) y--; //se
+				} else {
+					x++; if (Math.abs(x) % 2 == 0) y++; //ne
+				}
+			}
+			count++;
+		}
+		//Go to y = 0
+		while (y != 0) {
+			if (y > 0) {
+				y--; //s
+			} else {
+				y++; //n
+			}
+			count++;
+		}
+		System.out.println(count);
+	}
+
+}
